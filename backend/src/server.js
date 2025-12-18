4fb2c914af4dc12930f3129919fc8838cf32f867
Rayan Khan <rayan4khan1@gmail.com>
2025-12-18 12:00:21 +0500

    initial commit

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,21 +1,30 @@
 import express from "express";
 import { config } from "dotenv";
-import { resolve } from "path";
+import { resolve, join } from "path";
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import { connectDB } from "./lib/db.js";
 
 config();
+
 const app = express();
 const __dirname = resolve();
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+// Connect to MongoDB before starting the server
+await connectDB();
+
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
-//make ready for deployment
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-  app.get("*", (res, req) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(join(__dirname, "../frontend/dist")));
+  app.get("*", (req, res) => {
+    res.sendFile(join(__dirname, "../frontend/dist/index.html"));
   });
 }
-app.listen(PORT, () => console.log(`Server is running on port  ${PORT} `));
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
