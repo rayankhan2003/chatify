0c22c4e04c0d83df23eff2741f6784ecfd4aceba
Rayan Khan <rayan4khan1@gmail.com>
2025-12-29 16:46:11 +0500

    feat : update user profile method

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,10 +1,19 @@
 import { Router } from "express";
-import { signup, login, logout } from "../controllers/auth.controller.js";
+import {
+  signup,
+  login,
+  logout,
+  updateProfile,
+} from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
-
+router.post("/update-profile", protectRoute, updateProfile);
+router.get("/check", protectRoute, (req, res) =>
+  res.status(200).json(req.user)
+);
 export default router;
